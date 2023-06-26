0da0de95c1fa81fa18eafb05f3e86697b5a8bb3e
Agustin Galier <agusstingalier@gmail.com>
2023-06-26 18:32:26 -0300

    Fianlizamos con playground intermedio

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
 from inicio import views
 
+app_name='inicio'
+
 urlpatterns = [
-    path('',views.inicio),
-    path('prueba/', views.prueba),
-    path('segunda-vista/', views.segunda_vista),
-    path('fecha-actual/', views.fecha_actual),
-    path('saludar/', views.saludar),
-    path('bienvenide/<str:nombre>/<str:apellido>/', views.bienvenide),
-    path('crear-perro/<str:nombre>/<int:edad>/', views.crear_perro)  
+    path('',views.inicio,name='inicio'),
+    path('prueba/', views.prueba,name='prueba'),
+    path('segunda-vista/', views.segunda_vista,name='segunda_vista'),
+    path('fecha-actual/', views.fecha_actual,name='fecha_actual'),
+    path('saludar/', views.saludar,name='saludar'),
+    path('bienvenide/<str:nombre>/<str:apellido>/', views.bienvenide,name='bienvenide'),
+    #v1 crear perro
+    # path('crear-perro/<str:nombre>/<int:edad>/', views.crear_perro,name='crear_perro')  
+    #v2 crear perro
+    path('perros/', views.listar_perros,name='listar_perros'),  
+    path('perros/crear/', views.crear_perro,name='crear_perro'),  
 ]
